1636001b24d5c5adae7df2776123c8df5e73a3ba
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2019-09-26 09:08:01 +0200

    add test

---
+++ tools/analyzer_plugin/test/integration/indent_of_statements/inside_function_expression_in_list.dart
@@ -0,0 +1,23 @@
+f1() {
+  <dynamic>[
+    () {
+      null;
+      // LINT :8
+       null;
+    },
+  ];
+  <dynamic>{
+    () {
+      null;
+      // LINT :8
+       null;
+    },
+  };
+  <String, dynamic>{
+    '': () {
+      null;
+      // LINT :8
+       null;
+    },
+  };
+}
